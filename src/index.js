c974ca39ec90bc014b9c0d268db9217d5863a521
zapster <findingslimes@gmail.com>
2024-11-18 11:00:31 -0500

    Dynamically loading data!

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import * as $ from "jquery";
-import { signUserUp, signUserOut, signUserIn, changeRoute } from "./model";
+import { signUserUp, signUserOut, signUserIn, changeRoute, checkRequired, getData } from "./model";
+
+const userRecipes = {
+  "User made": {
+    imageUrl:
+      "https://gratisography.com/wp-content/uploads/2024/10/gratisography-cool-cat-800x525.jpg",
+    description: "this was ",
+    cookTime: "1min",
+    serveSize: "1",
+  },
+};
 
 function initListeners() {
   $(window).on(`hashchange`, changeRoute);
+  $("#mobileHamburger").on("click", function () {
+    if ($("#hamburgerCallapsible").css("height") == "0px") {
+      $("#hamburgerCallapsible").css("height", "302px");
+    } else {
+      $("#hamburgerCallapsible").css("height", "0px");
+    }
+  });
   changeRoute();
 
   $(".signoutBtn").on("click", (e) => {
@@ -15,6 +32,83 @@ function initListeners() {
 export function initListenersByPage(pageName) {
   // console.log("initListeners for page", pageName);
   switch (pageName) {
+    case "home":
+      break;
+    case "browse":
+      getData(function (data) {
+        $.each(data, function (i, val) {
+          console.log(i, val);
+          $("#recipeList").append(createRecipeElementLiteral(i, val));
+        });
+      });
+
+      break;
+    case "yourRecipes":
+      $.each(userRecipes, function (i, val) {
+        console.log(i, val);
+        $("#recipeList").append(createRecipeElementLiteral(i, val, true));
+      });
+
+      break;
+    case "createRecipe":
+      $("#ingredientAdd").on("click", (e) => {
+        const parent = e.currentTarget.parentElement.children[1];
+        const count = parent.children.length + 1;
+        const html =
+          '<div class="input-container">' +
+          `<input placeholder="" id="ingredient${count}" type="text" />` +
+          `<label for="name">Ingredient #${count}</label>` +
+          "</div>";
+        $(parent).append(html);
+      });
+      $("#ingredientMinus").on("click", (e) => {
+        const parent = e.currentTarget.parentElement.children[1];
+
+        const count = parent.children.length;
+        parent.children[count - 1].remove();
+      });
+      $("#instructionAdd").on("click", (e) => {
+        const parent = e.currentTarget.parentElement.children[1];
+        const count = parent.children.length + 1;
+        const html =
+          '<div class="input-container">' +
+          `<input placeholder="" id="instruction${count}" type="text" />` +
+          `<label for="name">Instruction #${count}</label>` +
+          "</div>";
+        $(parent).append(html);
+      });
+      $("#instructionMinus").on("click", (e) => {
+        const parent = e.currentTarget.parentElement.children[1];
+
+        const count = parent.children.length;
+        parent.children[count - 1].remove();
+      });
+      $("#submitBtn").on("click", (e) => {
+        if (!checkRequired("createRecipeForm")) return;
+
+        e.preventDefault();
+        let recipe = {
+          recipeName: $("#recipeName").val(),
+          recipeImageURL: $("#recipeImage").val(),
+          ingredients: [],
+          instructions: [],
+        };
+
+        $("#ingredients input").each(function () {
+          recipe.ingredients.push($(this).val());
+        });
+        $("#instructions input").each(function () {
+          recipe.instructions.push($(this).val());
+        });
+
+        $("form input").each(function () {
+          if ($(this).attr("type") != "submit") $(this).val("");
+        });
+
+        userRecipes.push(recipe);
+        console.log(userRecipes);
+      });
+      break;
     case "login":
       $("#signup-submit").on("click", (e) => {
         e.preventDefault();
@@ -38,3 +132,37 @@ export function initListenersByPage(pageName) {
 $(function () {
   initListeners();
 });
+
+function createRecipeElementLiteral(recipeName, recipeJson, includeEditBtns = false) {
+  let viewBtnHtml = ``;
+  let editBtnsHtml = ``;
+  if (includeEditBtns) {
+    viewBtnHtml = `<button class="button viewbtn"><span>View</span></button>`;
+    editBtnsHtml = `<div class="yourRecipeBtns">
+              <div class="button">Edit Recipe</div>
+              <div class="button">Delete</div>
+            </div>`;
+  }
+  let recipeHtml = `<div class="recipe">
+  ${editBtnsHtml}
+            <div class="recipe-coverImg">
+              <div class="background" style="background-image: url(${recipeJson.imageUrl})"></div>
+              ${viewBtnHtml}
+            </div>
+            <div class="recipe-content">
+              <h1>${recipeName}</h1>
+              <p>
+                ${recipeJson.description}
+              </p>
+              <div class="grid-container">
+                <div class="grid-item right"><img src="../assets/images/time.svg" alt="" /></div>
+                <div class="grid-item"><p>${recipeJson.cookTime}</p></div>
+                <div class="grid-item right">
+                  <img src="../assets/images/servings.svg" alt="" />
+                </div>
+                <div class="grid-item"><p>${recipeJson.serveSize} servings</p></div>
+              </div>
+            </div>
+          </div>`;
+  return recipeHtml;
+}
--- src/model.js
+++ src/model.js
@@ -20,7 +20,7 @@ onAuthStateChanged(auth, (user) => {
     $(".signoutBtn").show();
     $(".loginBtn").hide();
     $(".yourRecipes").show();
-    $(".createRecipe").hide();
+    $(".createRecipe").show();
   } else {
     $("#status").html("not signed in");
     $(".signoutBtn").hide();
@@ -29,6 +29,21 @@ onAuthStateChanged(auth, (user) => {
     $(".createRecipe").show();
   }
 });
+
+export function getData(callback) {
+  $.getJSON("data/data.json", function (data) {
+    callback(data);
+  })
+    .done(function () {
+      console.log("second success");
+    })
+    .fail(function () {
+      console.log("error");
+    })
+    .always(function () {
+      console.log("complete");
+    });
+}
 
 export function signUserUp(firstName, lastName, email, password) {
   // console.log(`${firstName}, ${lastName}, ${email}, ${password}`);
@@ -78,7 +93,6 @@ export function changeRoute(e) {
     pageID = `home`;
   }
   $.get(`pages/${pageID}.html`, function (data) {
-    // console.log(`data ` + data);
     $(`#app`).html(data);
   })
     .done(function () {
@@ -86,13 +100,16 @@ export function changeRoute(e) {
     })
     .fail(function (error) {
       $.get(`pages/404.html`, function (data) {
-        $(`#app`).html(data);
+        $(`#app`).html(
+          `<!DOCTYPE html><style>.error{font-family:monospace;margin-top:200px;padding:0 50px}.error .box{margin:0 auto;background-color:rgba(128,128,128,.356);width:fit-content;padding:30px 30px;width:100%;max-width:500px}.error .box span{font-family:sans-serif;font-size:15px}.error .box span{font-size:15px}</style><div class='error'><div class='box'><h1>Error</h1><p id='errorDetails'></p><p id='errorCode'></p></div></div>`
+        );
       }).done(function () {
         $(`#errorDetails`).html(`The page you are looking for '${pageID}' can not be found.`);
         $(`#errorCode`).html(`${error.status}: ${error.statusText}`);
       });
     })
     .always(function () {
+      //Add the active class to anchor tags with the same pageID as an href
       $(`a`).each(function () {
         let aHref = $(this).attr(`href`).replace(`#`, ``);
         if (aHref == pageID) {
@@ -102,4 +119,30 @@ export function changeRoute(e) {
         }
       });
     });
+}
+
+export function checkRequired(id) {
+  let allAreFilled = true;
+  document
+    .getElementById(id)
+    .querySelectorAll("[required]")
+    .forEach(function (i) {
+      if (!allAreFilled) return;
+      if (i.type === "radio") {
+        let radioValueCheck = false;
+        document
+          .getElementById("myForm")
+          .querySelectorAll(`[name=${i.name}]`)
+          .forEach(function (r) {
+            if (r.checked) radioValueCheck = true;
+          });
+        allAreFilled = radioValueCheck;
+        return;
+      }
+      if (!i.value) {
+        allAreFilled = false;
+        return;
+      }
+    });
+  return allAreFilled;
 }
